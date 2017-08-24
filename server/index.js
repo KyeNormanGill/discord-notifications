473a95e94d67db07e4e8c984c90144d141aeec79
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-08-25 04:06:22 +1000

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const { Server, OPEN } = require('uws');
+const { Client } = require('discord.js');
+const { port, token } = require('./config.json');
+
+const client = new Client();
+const server = new Server({ port }, () => {
+	server.on('connection', connection => {
+		console.log('Connection made!');
+		console.log(`There are ${server.clients.length} connections open!`);
+		connection.send(data('readyCache', messages));
+	});
+});
+const triggerWords = ['add', 'words', 'here'];
+const messages = [];
+
+server.once('listening', () => {
+	console.log(`Websocket check!`);
+});
+
+client.once('ready', () => {
+	console.log(`Discord check!`);
+});
+
+client.on('message', message => {
+	if (message.channel.type === 'dm') return;
+	if (triggerWords.some(word => message.content.toLowerCase().includes(word))) {
+		messages.unshift({
+			content: message.content, 
+			author: message.author.username,
+			avatar: message.author.displayAvatarURL,
+			where: `${message.guild.name}#${message.channel.name}`,
+			timestamp: message.createdAt
+		});
+		if (messages.length > 30) {
+			messages.pop();
+		}
+		if (server.clients.length < 1) return;
+		server.clients.forEach(client => {
+			if (client.readyState === OPEN) client.send(data('message', messages));
+		});
+	}
+});
+
+client.login(token).catch(console.error);
+
+function data(event, data) {
+	return JSON.stringify({ event, data })
+}
---
+++ ui/notifications.js
@@ -0,0 +1,32 @@
+const socket = new WebSocket('your ws server');
+const messageSound = new Audio('./sounds/message.mp3');
+
+const vue = new Vue({
+	el: '#vue-wrapper',
+	data: {
+		blocks: []
+	},
+	methods: {
+		cancelTTS() {
+			window.speechSynthesis.cancel();
+			setTimeout(() => {
+				window.speechSynthesis.speak(new SpeechSynthesisUtterance('yes'));
+			}, 100);
+		}
+	}
+});
+
+socket.onmessage = (e) => {
+	const message = JSON.parse(e.data);
+
+	if (message.event === 'message') {
+		vue.blocks = message.data;
+		messageSound.play().then(() => setTimeout(function() {
+			window.speechSynthesis.speak(new SpeechSynthesisUtterance(`${message.data[0].author} said ${message.data[0].content}`));
+		}, 2000))
+	} else
+	if (message.event === 'readyCache') {
+		vue.blocks = message.data;
+	}
+}
+
